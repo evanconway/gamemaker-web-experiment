b5e259cb28a4f08045eba190d6b734f291c223c4
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-11-04 23:28:54 -0400

    add self signed certificate for https

--- server/src/http.ts
+++ server/src/http.ts
@@ -1,7 +1,13 @@
 import http from 'http';
+import https from 'https';
+const fs = require('node:fs');
 
+const options = {
+  key: fs.readFileSync('private_keys/private.key'),
+  cert: fs.readFileSync('private_keys/signed_cert.crt'),
+};
 // Create a local server to receive data from
-const server = http.createServer((req, res) => {
+const server = https.createServer(options, (req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
     data: 'Hello World!',
