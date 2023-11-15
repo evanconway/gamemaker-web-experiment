5d1af7bd62071f54cc6927d22393418083f5febd
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-11-14 20:33:34 -0500

    add escure flag to env

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,16 +1,25 @@
 import { WebSocketServer, Server } from 'ws';
 import fs from 'fs';
+import dotenv from "dotenv";
 import game, { ClientState, ReceivedEvent, SendPlayerData } from "./game";
-
 import https from 'https';
 import http from 'http';
 
+dotenv.config();
+
+const secure =  process.env['SECURE'] === 'true';
+const pathToKey = process.env['PATH_TO_KEY'];
+const pathToCert = process.env['PATH_TO_CERT'];
+
+if (secure === undefined) throw new Error('SCURE not defined in .env');
+if (pathToKey === undefined) throw new Error('PATH_TO_KEY not defined in .env');
+if (pathToCert === undefined) throw new Error('PATH_TO_CERT not defined in .env');
+
 const startSocketServer = () => {
-    const secure = true;
     const port = secure ? 443 : 5000;
-    
-    const privateKey = fs.readFileSync('ssl-cert/private.key', 'utf8');
-    const certificate = fs.readFileSync('ssl-cert/cert.crt', 'utf8');
+
+    const privateKey = fs.readFileSync(pathToKey, 'utf8');
+    const certificate = fs.readFileSync(pathToCert, 'utf8');
     const credentials = { key: privateKey, cert: certificate };
 
     //pass in your credentials to create an https server
